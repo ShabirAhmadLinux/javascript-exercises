796c04bb1448c274aab966486711f56a771bba9e
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-09-19 16:51:11 +0000

    Complete 07-reverseString excercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+ let reverseString='';
+ for(let i=string.length-1;i>-1;i--){
+    reverseString+=string.at(i);
+ }
+ return reverseString;
 };
 
 // Do not edit below this line
