3594e9da28c3148d97fe73966587d25ce845a9ae
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-09-22 07:08:16 +0000

    Complete 08_removeFromArray excercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...nums) {
+    for(numIndex=0;numIndex<nums.length;numIndex++){
+        let index=array.findIndex(item=>item===nums[numIndex]);
+        if(index===-1)
+            continue;
+        else{
+          array.splice(index,1);
+          numIndex--; //To recheck the same number in nums array
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
