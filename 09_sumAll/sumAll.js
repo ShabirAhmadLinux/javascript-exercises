d053252f2d5905e0aef59b08f46b951ea3b95002
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-09-22 07:46:51 +0000

    Complete 09_sumAll Exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0 || Math.floor(a) !== a || Math.floor(b) !== b)
+        return "ERROR";
+    let sum = 0;
+    let start = a < b ? a : b;
+    let end = a > b ? a : b;
+    while (start <= end) {
+        sum += start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
