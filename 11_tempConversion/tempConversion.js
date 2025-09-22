02a8e0b94100a03bff5e917cdbba8328ddc8a43e
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-09-22 09:05:59 +0000

    Complete 11_tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let tempInCelsius=(fahrenheit-32)/1.8;
+  return parseFloat(tempInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let tempInFahrenheit=(celsius*1.8)+32;
+  return parseFloat(tempInFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
