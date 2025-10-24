5deb97501a50760159d5b99bd6c6b8691614054e
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-10-24 15:29:44 +0000

    Complete 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let palindrome="";
+  let string1="";
+  let string2="";
+  for(let i=str.length-1;i>=0;i--){
+    if((str.charAt(i)>='A'&&str.charAt(i)<='z')||(str.charAt(i)>='0'&&str.charAt(i)<='9')){
+       string1+=str.charAt(i);
+       string2=str.charAt(i)+string2;
+    }
+  }
+  return(string1.toLowerCase()===string2.toLowerCase());
 };
 
 // Do not edit below this line
