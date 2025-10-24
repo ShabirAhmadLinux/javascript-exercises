631feea8de61becfc7cf1197ad5ae0509898a8a7
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-10-24 15:43:28 +0000

    Complete 14_fibonacci problem

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  let first=0;
+  let second=1;
+  let third;
+  for(let i=0;i<a;i++){
+    third=first+second;
+    first=second;
+    second=third;
+  }
+  return a<0?"OOPS":first;
 };
 
 // Do not edit below this line
