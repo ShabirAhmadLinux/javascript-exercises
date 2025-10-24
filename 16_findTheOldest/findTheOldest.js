f93db70c27b97ca6ff1aa27ad66833adddf1d007
Shabir Ahmad <shabirahmadubuntu@gmail.com>
2025-10-24 16:35:04 +0000

    Complete 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let person=arr.sort((a,b)=>{
+    const NOW_DATE= new Date();
+    const NOW_YEAR=NOW_DATE.getFullYear();
+    if(!("yearOfDeath" in a)){
+        a.yearOfDeath=NOW_YEAR;
+    }
+    if(!("yearOfDeath" in b)){
+        b.yearOfDeath=NOW_YEAR;
+    }
+    return (b.yearOfDeath-b.yearOfBirth)-(a.yearOfDeath-a.yearOfBirth);
+  });
+  return person[0];
 };
 
 // Do not edit below this line
